7bda9ee500930e951848aa42408edf1203f94e94
ekoramadhanu <ekorama97@student.ub.ac.id>
2020-11-26 01:02:08 +0700

    redesign change password

--- src/views/ChangePassword.vue
+++ src/views/ChangePassword.vue
@@ -21,8 +21,91 @@
                 v-model="newPassword"
                 :rules="newPasswordRules"
                 @click:append="changeShowNewPassword()"
+                @input="checkedFormatPassword()"
+                @blur="checkedFormatPassword()"
+                @focus="checkedFormatPassword()"
                 required
               />
+              <div class="d-flex">
+                <p
+                  :class="
+                    checkNumber === false
+                      ? 'text-caption mb-0 mr-3 error--text'
+                      : 'text-caption mb-0 mr-3 success-text'
+                  "
+                >
+                  <span class="font-family"> kata sandi harus ada angka </span>
+                </p>
+                <v-icon v-if="checkNumber === false" size="15" color="error">
+                  $close
+                </v-icon>
+                <v-icon size="15" v-if="checkNumber === true" color="success">
+                  $check
+                </v-icon>
+              </div>
+              <div class="d-flex">
+                <p
+                  :class="
+                    checkSymbol === false
+                      ? 'text-caption mb-0 mr-3 error--text'
+                      : 'text-caption mb-0 mr-3 success-text'
+                  "
+                >
+                  <span class="font-family"> kata sandi harus ada simbol </span>
+                </p>
+                <v-icon v-if="checkSymbol === false" size="15" color="error">
+                  $close
+                </v-icon>
+                <v-icon size="15" v-if="checkSymbol === true" color="success">
+                  $check
+                </v-icon>
+              </div>
+              <div class="d-flex">
+                <p
+                  :class="
+                    checkUppercase === false
+                      ? 'text-caption mb-0 mr-3 error--text'
+                      : 'text-caption mb-0 mr-3 success-text'
+                  "
+                >
+                  <span class="font-family">
+                    kata sandi harus diawali huruf kapital
+                  </span>
+                </p>
+                <v-icon v-if="checkUppercase === false" size="15" color="error">
+                  $close
+                </v-icon>
+                <v-icon
+                  size="15"
+                  v-if="checkUppercase === true"
+                  color="success"
+                >
+                  $check
+                </v-icon>
+              </div>
+              <div class="d-flex">
+                <p
+                  :class="
+                    checkLowercase === false
+                      ? 'text-caption mb-0 mr-3 error--text'
+                      : 'text-caption mb-0 mr-3 success-text'
+                  "
+                >
+                  <span class="font-family">
+                    kata sandi harus ada huruf kecil
+                  </span>
+                </p>
+                <v-icon v-if="checkLowercase === false" size="15" color="error">
+                  $close
+                </v-icon>
+                <v-icon
+                  size="15"
+                  v-if="checkLowercase === true"
+                  color="success"
+                >
+                  $check
+                </v-icon>
+              </div>
               <v-text-field
                 label="Ulangi Kata Sandi Baru"
                 :type="showRePassword ? 'text' : 'password'"
@@ -86,31 +169,57 @@ export default {
     password: '',
     passwordRules: [
       (v) => !!v || 'Kata Sandi Tidak Boleh Kosong',
-      (v) => (v.length >= 6 && v.length <= 12) || 'Kata Sandi Harus (6-12)',
+      (v) => (v.length >= 8 && v.length <= 12) || 'Kata Sandi Harus (8-12)',
     ],
     newPassword: '',
     newPasswordRules: [
       (v) => !!v || 'Kata Sandi Baru Tidak Boleh Kosong',
-      (v) => (v.length >= 6 && v.length <= 12) || 'Kata Sandi Baru Harus (6-12)',
+      (v) => (v.length >= 8 && v.length <= 12) || 'Kata Sandi Baru Harus (8-12)',
     ],
     rePassword: '',
     rePasswordRules: [
       (v) => !!v || 'Ulangi Kata Sandi Tidak Boleh Kosong',
-      (v) => (v.length >= 6 && v.length <= 12) || 'Ulangi Kata Sandi Harus (6-12)',
+      (v) => (v.length >= 8 && v.length <= 12) || 'Ulangi Kata Sandi Harus (8-12)',
     ],
     showPassword: false,
     showNewPassword: false,
     showRePassword: false,
     role: '',
     loading: false,
+    checkUppercase: null,
+    checkLowercase: null,
+    checkSymbol: null,
+    checkNumber: null,
   }),
   methods: {
     save() {
+      if (this.checkUppercase === null) {
+        this.checkUppercase = false;
+      }
+      if (this.checkLowercase === null) {
+        this.checkLowercase = false;
+      }
+      if (this.checkSymbol === null) {
+        this.checkSymbol = false;
+      }
+      if (this.checkNumber === null) {
+        this.checkNumber = false;
+      }
       if (this.$refs.form.validate()) {
         if (this.rePassword !== this.newPassword) {
           this.hasSaved = true;
           this.status = false;
           this.message = 'kata sandi baru dan ulangi kata sandi tidak sama';
+          this.icon = '$warning';
+        } else if (
+          this.checkUppercase === false
+          || this.checkLowercase === false
+          || this.checkSymbol === false
+          || this.checkNumber === false
+        ) {
+          this.hasSaved = true;
+          this.status = false;
+          this.message = 'kata sandi anda tidak memenuhi syarat';
           this.icon = '$warning';
         } else {
           this.loading = true;
@@ -179,6 +288,30 @@ export default {
     },
     changeShowRePassword() {
       this.showRePassword = !this.showRePassword;
+    },
+    checkedFormatPassword() {
+      const lowerCaseRegex = '(?=.*[a-z])';
+      const upperCaseRegex = '([A-Z])';
+      const symbolsRegex = '(?=.*[!@#$%^&*])';
+      const numericRegex = '(?=.*[0-9])';
+      if (new RegExp(`^${upperCaseRegex}`).test(this.newPassword)) {
+        this.checkUppercase = true;
+      }
+      if (new RegExp(`^${lowerCaseRegex}`).test(this.newPassword)) {
+        this.checkLowercase = true;
+      }
+      if (new RegExp(`^${symbolsRegex}`).test(this.newPassword)) {
+        this.checkSymbol = true;
+      }
+      if (new RegExp(`^${numericRegex}`).test(this.newPassword)) {
+        this.checkNumber = true;
+      }
+      if (this.newPassword === '' || this.newPassword === null) {
+        this.checkUppercase = false;
+        this.checkLowercase = false;
+        this.checkSymbol = false;
+        this.checkNumber = false;
+      }
     },
   },
   created() {
